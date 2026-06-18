e6524c7e6eec340e9bf47a48e95a5f6aef770e67
BOTREL Kilian <botrel.kilian1999@gmail.com>
2026-06-19 01:31:03 +0200

    chore: add firefox specific data to manifest on build

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ echo "Created: $CHROME_OUTPUT_FILE"
 
 FIREFOX_OUTPUT_FILE="$OUTPUT_DIR/${VERSION}-firefox.zip"
 
-jq '. + {"background": {"scripts": ["background/background.js"]}}' manifest.json > "$TMP_DIR/manifest.json"
+jq '. + {"background": {"scripts": ["background/background.js"]}, "browser_specific_settings": {"gecko": {"id": "osu-idle-score-extractor@demonwaves", "data_collection_permissions": {"required": ["none"]}}}}' manifest.json > "$TMP_DIR/manifest.json"
 zip -j "$FIREFOX_OUTPUT_FILE" "$TMP_DIR/manifest.json"
 zip -r "$FIREFOX_OUTPUT_FILE" "${BUNDLE_FILES[@]}"
 echo "Created: $FIREFOX_OUTPUT_FILE"
